a920dc19d9045243f646c004c3260212cec8034b
akash0107 <debnathakash.27@gmail.com>
2023-09-04 09:41:14 +0530

    some modifications regarding login page

--- src/main/java/pages/SearchPages.java
+++ src/main/java/pages/SearchPages.java
@@ -4,6 +4,8 @@ public class SearchPages {
 	
 	public void searchOps() {
 		System.out.println("Search ops begins");
+		System.out.println("Searching....");
+		System.out.println("Byee.........");
 	}
 
 }
--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -1,5 +1,11 @@
 package tests;
 
+import pages.LoginPage;
+
 public class LoginPageTest {
 
+	public void loginOps() {
+		
+		LoginPage.lp();
+	}
 }
