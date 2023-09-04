1107e0906959c423df4f31e844f7fbfc23207495
akash0107 <debnathakash.27@gmail.com>
2023-09-04 20:48:11 +0530

    cart feature development continued....

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -5,5 +5,6 @@ public class CartPage {
 	public void getCart() {
 		System.out.println("The no of item in the cart is 1");
 		System.out.println("The number of item saved is 3");
+		System.out.println("The number of items in the wish list are 5");
 	}
 }
