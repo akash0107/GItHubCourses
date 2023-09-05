de6d429560f7bc4c77d86b23e3c5c753fdb771cd
akash0107 <debnathakash.27@gmail.com>
2023-09-05 20:09:44 +0530

    Cart page update

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -3,8 +3,10 @@ package pages;
 public class CartPage {
 
 	public void getCart() {
-		System.out.println("The no of item in the cart is 1");
+		String user="Akash";
+		System.out.println("Cart item is 1");
 		System.out.println("The number of item saved is 3");
 		System.out.println("The number of items in the wish list are 5");
+		System.out.println("This cart page belongs to user: "+user);
 	}
 }
